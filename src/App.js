d8a48c0b7f6824fa28cf7287416cf796befce3af
Gipsy Martinez <gipsy.martinez@gmail.com>
2019-06-19 16:11:07 -0400

    Todo list component created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import List from "./List";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      term: ""
+    };
+  }
+
+  onChange = event => {
+    this.setState({ term: event.target.value });
+  };
+
+  onSubmit = event => {
+    event.preventDefault();
+    this.setState({
+      term: "",
+      items: [...this.state.items, this.state.term]
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="" onSubmit={this.onSubmit}>
+          <input value={this.state.term} onChange={this.onChange} />
+          <button>guardar</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
 }
 
 export default App;
